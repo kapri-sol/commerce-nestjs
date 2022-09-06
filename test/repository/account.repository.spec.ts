a8bdd29241031bc7530b2a8e9f897763d3a8aa6c
kapri <44130355+kapri-sol@users.noreply.github.com>
2022-09-07 08:55:36 +0900

    custom account repository 주입

--- test/repository/account.repository.spec.ts
+++ test/repository/account.repository.spec.ts
@@ -1,19 +1,22 @@
 import { Account } from 'src/entity/account.entity';
 import { getTestDatasource } from 'test/util/database.util';
-import { DataSource, Repository } from 'typeorm';
+import { DataSource } from 'typeorm';
 import { faker } from '@faker-js/faker';
 import * as bcrypt from 'bcrypt';
+import { AccountRepository } from 'src/module/account/account.repository';
 
 describe('Account Repository', () => {
   let datasource: DataSource;
-  let accountRepository: Repository<Account>;
+  let accountRepository: AccountRepository;
 
   beforeAll(async () => {
     datasource = getTestDatasource({ entities: [Account] });
 
     await datasource.initialize();
 
-    accountRepository = datasource.getRepository(Account);
+    accountRepository = new AccountRepository(
+      datasource.getRepository(Account),
+    );
   });
 
   afterEach(async () => {
@@ -29,13 +32,14 @@ describe('Account Repository', () => {
       // given
       const email = faker.internet.email();
       const password = faker.internet.password();
-      const account = Account.of(email, password);
+      const phone = faker.phone.number('+82 10-####-####');
+      const account = Account.of(email, phone, password);
 
       // when
       await accountRepository.save(account);
 
       // then
-      expect(account.accountId).toStrictEqual(expect.any(BigInt));
+      expect(account.id).toStrictEqual(expect.any(BigInt));
       expect(account.email).toBe(email);
       expect(account.password).toStrictEqual(expect.any(String));
     });
@@ -44,7 +48,8 @@ describe('Account Repository', () => {
       // given
       const email = faker.internet.email();
       const password = faker.internet.password();
-      const account = Account.of(email, password);
+      const phone = faker.phone.number('+82 10-####-####');
+      const account = Account.of(email, phone, password);
 
       // when
       await accountRepository.save(account);
@@ -57,6 +62,7 @@ describe('Account Repository', () => {
       // then
       expect(isRightPassword).toBe(true);
     });
+  });
 
     describe('update', () => {
       it('Account의 정보를 수정한다.', async () => {
