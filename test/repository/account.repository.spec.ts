96bb34068f765c33d88c1eabec4024048cb5e69b
kapri <44130355+kapri-sol@users.noreply.github.com>
2022-09-07 09:07:22 +0900

    account repository 수정 테스트 추가

--- test/repository/account.repository.spec.ts
+++ test/repository/account.repository.spec.ts
@@ -64,12 +64,100 @@ describe('Account Repository', () => {
     });
   });
 
-    describe('update', () => {
-      it('Account의 정보를 수정한다.', async () => {
-        // given
-        // when
-        // then
-      });
+  describe('update', () => {
+    it('Account의 정보를 수정한다.', async () => {
+      // given
+      const email = faker.internet.email();
+      const password = faker.internet.password();
+      const phone = faker.phone.number('+82 10-####-####');
+      const account = Account.of(email, phone, password);
+      const updatePhone = faker.phone.number();
+
+      // when
+      account.phone = updatePhone;
+      await accountRepository.save(account);
+
+      console.log(account.phone);
+      // then
+      expect(account.phone).toBe(updatePhone);
+    });
+
+    it('Account가 수정 될 때, hashPassword가 동작한다.', async () => {
+      const email = faker.internet.email();
+      const password = faker.internet.password();
+      const phone = faker.phone.number('+82 10-####-####');
+      const account = Account.of(email, phone, password);
+      const updatePassword = faker.internet.password();
+      await accountRepository.save(account);
+
+      // when
+      account.password = updatePassword;
+      await accountRepository.save(account);
+
+      // then
+      const isRightPassword = await bcrypt.compareSync(
+        updatePassword,
+        account.password,
+      );
+      expect(isRightPassword).toBe(true);
+    });
+  });
+
+  describe('find', () => {
+    it('id로 Account를 검색한다.', async () => {
+      // given
+      const email = faker.internet.email();
+      const password = faker.internet.password();
+      const phone = faker.phone.number('+82 10-####-####');
+      const account = Account.of(email, phone, password);
+      await accountRepository.save(account);
+
+      // when
+      const findAccount = await accountRepository.findOneById(account.id);
+
+      // then
+      expect(findAccount).toMatchObject(account);
+    });
+
+    it('email로 Account를 검색한다.', async () => {
+      // given
+      const email = faker.internet.email();
+      const password = faker.internet.password();
+      const phone = faker.phone.number('+82 10-####-####');
+      const account = Account.of(email, phone, password);
+      await accountRepository.save(account);
+
+      // when
+      const findAccount = await accountRepository.findOneByEmail(account.email);
+
+      // then
+      expect(findAccount).toMatchObject(account);
+    });
+
+    it('모든 Account를 검색한다.', async () => {
+      // given
+      const email = faker.internet.email();
+      const password = faker.internet.password();
+      const phone = faker.phone.number('+82 10-####-####');
+      const account = Account.of(email, phone, password);
+      await accountRepository.save(account);
+
+      const email2 = faker.internet.email();
+      const password2 = faker.internet.password();
+      const phone2 = faker.phone.number('+82 10-####-####');
+      const account2 = Account.of(email2, phone2, password2);
+      await accountRepository.save(account2);
+
+      // when
+      const findAccount = await accountRepository.findAll();
+
+      // then
+      expect(findAccount).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining(account),
+          expect.objectContaining(account2),
+        ]),
+      );
     });
   });
 });
