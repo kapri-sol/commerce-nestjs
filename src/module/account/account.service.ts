34c38afb6ee4cb01086742b8948a3ec56d489d6d
kapri.kim <kaprikim@dreamyouns.com>
2022-09-08 10:45:58 +0900

    account 반환 받기

--- src/module/account/account.service.ts
+++ src/module/account/account.service.ts
@@ -6,8 +6,19 @@ import { CreateAccountDto } from './dto/create.dto';
 export class AccountService {
   constructor(private readonly accountRepository: AccountRepository) {}
 
+  /**
+   * 계정을 생성한다.
+   *
+   * @param {CreateAccountDto} createAccountDto
+   * @return {*}  {Promise<bigint>}
+   * @memberof AccountService
+   */
   async createAccount(createAccountDto: CreateAccountDto): Promise<bigint> {
     const account = createAccountDto.toEntity();
+    const createAccount = await this.accountRepository.save(account);
+    return createAccount.id;
+  }
+
     await this.accountRepository.save(account);
     return account.id;
   }
