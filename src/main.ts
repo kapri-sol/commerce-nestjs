696d6f6b6c580c01932d51c790af8eb64c8ffd44
kapri.kim <ksm6902@gmail.com>
2022-12-12 15:14:02 +0900

    swagger 적용

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('E-Commerce')
+    .setDescription('Pracice E - Commerce Domain')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(4000);
 }
 bootstrap();
