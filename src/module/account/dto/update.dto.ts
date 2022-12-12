a0fa5c86154a4f707aea32e5f98da7445ae2281f
kapri <44130355+kapri-sol@users.noreply.github.com>
2022-12-12 12:35:52 +0900

    UpdateAccountDto 생성

--- src/module/account/dto/update.dto.ts
+++ src/module/account/dto/update.dto.ts
@@ -1,4 +1,10 @@
+import { IsPhoneNumber, IsString } from 'class-validator';
+
 export class UpdateAccountDto {
+  @IsString()
+  @IsPhoneNumber('KR')
   phone: string;
+
+  @IsString()
   password: string;
 }
