cae7b8b49f19e0e3acaf4d692865c41401337ab1
kapri.kim <ksm6902@gmail.com>
2022-12-12 18:39:35 +0900

    response api property 적용

--- src/module/account/dto/response.dto.ts
+++ src/module/account/dto/response.dto.ts
@@ -1,10 +1,11 @@
-import { PickType } from '@nestjs/swagger';
-import { Account } from '@src/entity/account.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class FindAccountResponseDto extends PickType(Account, [
-  'email',
-  'phone',
-]) {}
+export class FindAccountResponseDto {
+  @ApiProperty()
+  email: string;
+  @ApiProperty()
+  phone: string;
+}
 
 export class CreateAccountResponseDto {
   id: string;
