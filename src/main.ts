4975761e92eda93e5c3d8107902ec3112542cf2e
kapri.kim <ksm6902@gmail.com>
2022-12-12 18:39:08 +0900

    global validation pipe, serialization 적용

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   const config = new DocumentBuilder()
     .setTitle('E-Commerce')
