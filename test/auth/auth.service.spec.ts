bb81426374ed665debdf472d06c04944f8c6de3c
kapri.kim <ksm6902@gmail.com>
2022-12-13 18:43:10 +0900

    인증 단일 테스트 생성

---
+++ test/auth/auth.service.spec.ts
@@ -0,0 +1,63 @@
+import { faker } from '@faker-js/faker';
+import { Account } from '@src/entity/account.entity';
+import { AccountService } from '@src/module/account/account.service';
+import { AuthService } from '@src/module/auth/auth.service';
+import { anyString, instance, mock, when } from 'ts-mockito';
+
+describe('Auth Service', () => {
+  let mockedAccountService: AccountService;
+  let accountService: AccountService;
+  let authService: AuthService;
+
+  beforeAll(() => {
+    mockedAccountService = mock(AccountService);
+  });
+
+  beforeEach(() => {
+    accountService = instance(mockedAccountService);
+    authService = new AuthService(accountService);
+  });
+
+  describe('validate', () => {
+    it('비교하는 password가 일치한다.', async () => {
+      // given
+      const email = faker.internet.email();
+      const phone = faker.phone.number('+82 10-####-####');
+      const password = faker.internet.password();
+      const account = Account.of(email, phone, password);
+      await account.hashPassword();
+
+      when(mockedAccountService.findAccountByEmail(anyString())).thenResolve(
+        account,
+      );
+
+      // when
+      const isValidate = await authService.validateAccount(email, password);
+
+      // then
+      expect(isValidate).toBe(true);
+    });
+
+    it('비교하는 password가 일치하지 않는다.', async () => {
+      // given
+      const email = faker.internet.email();
+      const phone = faker.phone.number('+82 10-####-####');
+      const password = faker.internet.password();
+      const account = Account.of(email, phone, password);
+      await account.hashPassword();
+
+      when(mockedAccountService.findAccountByEmail(anyString())).thenResolve(
+        account,
+      );
+
+      // when
+      const isValidate = await authService.validateAccount(
+        email,
+        faker.internet.password(),
+      );
+
+      // then
+      expect(isValidate).toBe(false);
+    });
+  });
+});
