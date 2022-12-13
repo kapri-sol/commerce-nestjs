00e6e1b32d73cbbcddb619396d2e09e9b3bd93a7
kapri.kim <ksm6902@gmail.com>
2022-12-13 18:42:51 +0900

    인증 서비스 생성

---
+++ src/module/auth/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { AccountService } from '../account/account.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly accountService: AccountService) {}
+
+  /**
+   * 계정의 패스워드와 일치하는지 확인한다.
+   *
+   * @param {string} email
+   * @param {string} password
+   * @return {*}
+   * @memberof AuthService
+   */
+  async validateAccount(email: string, password: string) {
+    const account = await this.accountService.findAccountByEmail(email);
+
+    return account?.validatePassword(password);
+  }
+}
