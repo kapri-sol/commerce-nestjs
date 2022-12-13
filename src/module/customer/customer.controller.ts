fa6fa397ebf7e3cc6515b083af38dc89b0fae516
kapri.kim <ksm6902@gmail.com>
2022-12-13 18:42:20 +0900

    고객 컨트롤러 생성

---
+++ src/module/customer/customer.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { ParseBigintPipe } from '@src/pipe/parse-bigint.pipe';
+import { CustomerService } from './customer.service';
+import { FindCustomerResponseDto } from './dto/response.dto';
+
+@ApiTags('Customer')
+@Controller('customers')
+export class CustomerController {
+  constructor(private readonly customerService: CustomerService) {}
+
+  @Get(':customerId')
+  async findCustomer(
+    @Param('customerId', ParseBigintPipe) customerId: bigint,
+  ): Promise<FindCustomerResponseDto> {
+    const { name, address } = await this.customerService.findCustomerById(
+      customerId,
+    );
+
+    return {
+      name,
+      address,
+    };
+  }
+}
