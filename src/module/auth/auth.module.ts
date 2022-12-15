ea04cbfe78bdfc3df9d3094d118b4bc2c57af30b
kapri.kim <ksm6902@gmail.com>
2022-12-15 17:59:03 +0900

    passport property 설정

--- src/module/auth/auth.module.ts
+++ src/module/auth/auth.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { AccountModule } from '../account/account.module';
+import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [AccountModule, PassportModule],
+  imports: [
+    AccountModule,
+    PassportModule.register({
+      session: true,
+      property: 'account',
+    }),
+  ],
+  controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
