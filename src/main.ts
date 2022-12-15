1d623a7a9ef0d3d5aa684c686c3be2782cce4a73
kapri.kim <ksm6902@gmail.com>
2022-12-15 17:57:05 +0900

    express session 적용

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as session from 'express-session';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -23,6 +24,18 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+
+  app.use(
+    session({
+      name: 'auth',
+      secret: 'my-secret',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+      },
+    }),
+  );
 
   await app.listen(4000);
 }
