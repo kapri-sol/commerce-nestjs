46003f090c09169e01ad39412b9b7d26f44a2e0f
kapri.kim <ksm6902@gmail.com>
2022-12-20 16:28:02 +0900

    optional 처리

--- src/module/account/dto/update.dto.ts
+++ src/module/account/dto/update.dto.ts
@@ -1,10 +1,12 @@
-import { IsPhoneNumber, IsString } from 'class-validator';
+import { IsOptional, IsPhoneNumber, IsString } from 'class-validator';
 
 export class UpdateAccountDto {
   @IsString()
   @IsPhoneNumber('KR')
+  @IsOptional()
   phone: string;
 
   @IsString()
+  @IsOptional()
   password: string;
 }
