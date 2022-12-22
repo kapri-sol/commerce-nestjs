1a1ce6fe9435e04988760c14561028cb754427a7
kapri.kim <ksm6902@gmail.com>
2022-12-22 18:07:31 +0900

    auth service interface 생성

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,27 +1,7 @@
-import { Injectable } from '@nestjs/common';
-import { AccountService } from '../account/account.service';
+import { Request } from 'express';
 
-@Injectable()
-export class AuthService {
-  constructor(private readonly accountService: AccountService) {}
+export interface AuthService {
+  validateAccount(email: string, password: string);
 
-  /**
-   * 계정의 패스워드와 일치하는지 확인한다.
-   *
-   * @param {string} email
-   * @param {string} password
-   * @return {*}
-   * @memberof AuthService
-   */
-  async validateAccount(email: string, password: string) {
-    const account = await this.accountService.findAccountByEmail(email);
-
-    const validateResult = await account?.validatePassword(password);
-
-    return validateResult
-      ? {
-          id: account.id.toString(),
-        }
-      : null;
-  }
+  login(request: Request);
 }
