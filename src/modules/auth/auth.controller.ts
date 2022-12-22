2ceddd97c59c376efd5b17a5714c6046dc4bb113
kapri.kim <ksm6902@gmail.com>
2022-12-22 18:08:05 +0900

    auth controller에서 auth service inject

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,24 +2,32 @@ import {
   Controller,
   HttpCode,
   HttpStatus,
+  Inject,
+  Injectable,
   Post,
-  Request,
+  Req,
   UseGuards,
 } from '@nestjs/common';
 import { ApiBody, ApiTags } from '@nestjs/swagger';
 import { LocalAuthGuard } from '@src/utils/guard/local-auth.guard';
+import { AuthService } from './auth.service';
 import { LoginDto } from './dto/auth.dto';
 
 @ApiTags('Auth')
+@Injectable()
 @Controller('auth')
 export class AuthController {
+  constructor(
+    @Inject('AuthService') private readonly authService: AuthService,
+  ) {}
+
   @UseGuards(LocalAuthGuard)
   @ApiBody({
     type: LoginDto,
   })
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  login(@Request() req) {
-    req.session.account = req.account;
+  login(@Req() req) {
+    return this.authService.login(req);
   }
 }
