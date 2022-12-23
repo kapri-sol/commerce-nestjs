4c07b78a6d074263207f0f72a9cc17f2f1377427
kapri.kim <ksm6902@gmail.com>
2022-12-23 11:37:24 +0900

    리턴 타입 지정

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Request } from 'express';
 
 export interface AuthService {
-  validateAccount(email: string, password: string);
+  validateAccount(email: string, password: string): Promise<object>;
 
   login(request: Request);
 }
