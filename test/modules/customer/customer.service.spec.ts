8da4e871b62d073ba3fa8b06fa6dcb67149842a4
kapri.kim <ksm6902@gmail.com>
2022-12-23 11:38:50 +0900

    테스트 추가

--- test/modules/customer/customer.service.spec.ts
+++ test/modules/customer/customer.service.spec.ts
@@ -1,20 +1,39 @@
 import { faker } from '@faker-js/faker';
+import { NotFoundException } from '@nestjs/common';
 import { Account } from '@src/entities/account.entity';
 import { Customer } from '@src/entities/customer.entity';
-import { AccountRepository } from '@src/modules/account/account.query-repository';
+import { AccountQueryRepository } from '@src/modules/account/account.query-repository';
 import { CustomerRepository } from '@src/modules/customer/customer.repository';
 import { CustomerService } from '@src/modules/customer/customer.service';
 import { CreateCustomerDto } from '@src/modules/customer/dto/create.dto';
 import { plainToInstance } from 'class-transformer';
 import { anyOfClass, anything, instance, mock, when } from 'ts-mockito';
+import { Repository } from 'typeorm';
 
 describe('Customer Service', () => {
-  let mockedAccountRepository: AccountRepository;
-  let accountRepository: AccountRepository;
+  let mockedAccountRepository: Repository<Account>;
+  let accountRepository: Repository<Account>;
+  let mockedAccountQueryRepository: AccountQueryRepository;
+  let accountQueryRepository: AccountQueryRepository;
   let mockedCustomerRepository: CustomerRepository;
   let customerRepository: CustomerRepository;
   let customerService: CustomerService;
 
+  const initializeAccount = () => {
+    return Account.of(
+      faker.internet.email(),
+      faker.phone.number('+82 10-####-####'),
+      faker.internet.password(),
+    );
+  };
+
+  const initializeCustomer = () => {
+    return Customer.of(
+      faker.name.fullName(),
+      faker.address.streetAddress(),
+      initializeAccount(),
+    );
+  };
   const saveAccount = (account?: Partial<Account>) => {
     return plainToInstance(Account, {
       _id: account?.id || BigInt(Math.floor(Math.random() * 100)),
@@ -33,7 +52,8 @@ describe('Customer Service', () => {
   };
 
   beforeAll(() => {
-    mockedAccountRepository = mock(AccountRepository);
+    mockedAccountRepository = mock(Repository<Account>);
+    mockedAccountQueryRepository = mock(AccountQueryRepository);
     mockedCustomerRepository = mock(CustomerRepository);
   });
 
@@ -46,9 +66,10 @@ describe('Customer Service', () => {
     );
 
     accountRepository = instance(mockedAccountRepository);
+    accountQueryRepository = instance(mockedAccountQueryRepository);
     customerRepository = instance(mockedCustomerRepository);
     customerService = new CustomerService(
-      accountRepository,
+      accountQueryRepository,
       customerRepository,
     );
   });
@@ -63,7 +84,7 @@ describe('Customer Service', () => {
       );
       const createAccount = await accountRepository.save(account);
 
-      when(mockedAccountRepository.findOneById(anything())).thenResolve(
+      when(mockedAccountQueryRepository.findOneById(anything())).thenResolve(
         createAccount,
       );
 
@@ -82,6 +103,63 @@ describe('Customer Service', () => {
       );
 
       expect(customerId).toStrictEqual(expect.any(BigInt));
+    });
+  });
+
+  describe('findCustomerById', () => {
+    it('고객을 id로 검색한다.', async () => {
+      // given
+      const customer = initializeCustomer();
+      const createCustomer = await customerRepository.save(customer);
+
+      when(mockedCustomerRepository.findOneById(createCustomer.id)).thenResolve(
+        createCustomer,
+      );
+
+      // when
+      const findCustomer = await customerService.findCustomerById(
+        createCustomer.id,
+      );
+
+      // then
+      expect(findCustomer.id).toBe(createCustomer.id);
+      expect(findCustomer.name).toBe(createCustomer.name);
+      expect(findCustomer.address).toBe(createCustomer.address);
+    });
+
+    it('id로 검색된 고객이 없다면 NotFoundException을 반환한다.', async () => {
+      // given
+      const customerId = BigInt(1);
+
+      when(mockedCustomerRepository.findOneById(customerId)).thenResolve(null);
+
+      // when
+      const findCustomer = () => customerService.findCustomerById(customerId);
+
+      // then
+      expect(findCustomer).rejects.toThrowError(NotFoundException);
+    });
+  });
+
+  describe('updateCustomer', () => {
+    it('고객을 id로 검색한다.', async () => {
+      // given
+      const customer = initializeCustomer();
+      const createCustomer = await customerRepository.save(customer);
+
+      when(mockedCustomerRepository.findOneById(createCustomer.id)).thenResolve(
+        createCustomer,
+      );
+
+      // when
+      const findCustomer = await customerService.findCustomerById(
+        createCustomer.id,
+      );
+
+      // then
+      expect(findCustomer.id).toBe(createCustomer.id);
+      expect(findCustomer.name).toBe(createCustomer.name);
+      expect(findCustomer.address).toBe(createCustomer.address);
     });
   });
 });
