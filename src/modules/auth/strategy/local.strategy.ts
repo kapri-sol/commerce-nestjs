3f88c9d7d291323bafd35048e066f7f25bb52e76
kapri.kim <ksm6902@gmail.com>
2022-12-23 11:37:52 +0900

    auth service interface injection

--- src/modules/auth/strategy/local.strategy.ts
+++ src/modules/auth/strategy/local.strategy.ts
@@ -1,11 +1,13 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from '../auth.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly authService: AuthService) {
+  constructor(
+    @Inject('AuthService') private readonly authService: AuthService,
+  ) {
     super({
       usernameField: 'email',
     });
