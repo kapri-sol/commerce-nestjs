bcf32c7632b496d9483b069795691b7b6dbf30d1
kapri <44130355+kapri-sol@users.noreply.github.com>
2022-12-25 22:27:29 +0900

    pg mem 을 이용한 테스트로 변경

--- test/modules/account/account.service.spec.ts
+++ test/modules/account/account.service.spec.ts
@@ -1,62 +1,71 @@
 import { AccountService } from '@src/modules/account/account.service';
 import { CreateAccountDto } from '@src/modules/account/dto/create-account.dto';
-import {
-  mock,
-  instance,
-  when,
-  anyOfClass,
-  anyString,
-  anything,
-} from 'ts-mockito';
 import { faker } from '@faker-js/faker';
 import { plainToInstance } from 'class-transformer';
 import { Account } from '@src/entities/account.entity';
 import { UpdateAccountDto } from '@src/modules/account/dto/update-account.dto';
 import { AccountQueryRepository } from '@src/modules/account/account.query-repository';
-import { Repository } from 'typeorm';
-import { NotFoundException } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { INestApplication, NotFoundException } from '@nestjs/common';
+import { getMemDateSource } from 'test/utils/pg-mem.util';
+import { IBackup } from 'pg-mem';
+import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Customer } from '@src/entities/customer.entity';
+import { Seller } from '@src/entities/seller.entity';
+import { Product } from '@src/entities/product.entity';
+import { Order } from '@src/entities/order.entity';
 
 describe('Account Service', () => {
-  let mockedAccountRepository: Repository<Account>;
+  let datasource: DataSource;
+  let backup: IBackup;
+  let app: INestApplication;
   let accountRepository: Repository<Account>;
-  let mockedAccountQueryRepository: AccountQueryRepository;
   let accountQueryRepository: AccountQueryRepository;
   let accountService: AccountService;
 
-  const initialAccount = plainToInstance(Account, {
-    _id: BigInt(1),
-    _email: faker.internet.email(),
-    _phone: faker.phone.number('+82 10-####-####'),
-    _password: faker.internet.password(),
-  });
+  const initializeAccount = () => {
+    const account = Account.of(
+      faker.internet.email(),
+      faker.phone.number('+82 10-####-####'),
+      faker.internet.password(),
+    );
 
-  const saveAccount = (account?: Partial<Account>) => {
-    return plainToInstance(Account, {
-      _id: account?.id || BigInt(Math.floor(Math.random() * 100)),
-      _email: account?.email,
-      _phone: account?.phone,
-      _password: account?.password,
-    });
+    return accountRepository.save(account);
   };
 
-  beforeAll(() => {
-    mockedAccountRepository = mock(Repository<Account>);
-    mockedAccountQueryRepository = mock(AccountQueryRepository);
+  beforeAll(async () => {
+    await getMemDateSource([Account, Customer, Seller, Product, Order]).then(
+      (data) => {
+        datasource = data.datasource;
+        backup = data.backup;
+      },
+    );
+
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot(),
+        TypeOrmModule.forFeature([Account, Customer, Seller]),
+      ],
+      providers: [AccountQueryRepository, AccountService],
+    })
+      .overrideProvider(DataSource)
+      .useValue(datasource)
+      .compile();
+
+    app = module.createNestApplication();
+
+    accountRepository = datasource.getRepository(Account);
+    accountQueryRepository = module.get<AccountQueryRepository>(
+      AccountQueryRepository,
+    );
+    accountService = module.get<AccountService>(AccountService);
+
+    await app.init();
   });
 
-  beforeEach(() => {
-    when(mockedAccountRepository.save(anyOfClass(Account))).thenResolve(
-      saveAccount(),
-    );
-
-    when(mockedAccountRepository.softRemove(anything())).thenResolve(undefined);
-
-    accountRepository = instance(mockedAccountRepository);
-    accountQueryRepository = instance(mockedAccountQueryRepository);
-    accountService = new AccountService(
-      accountRepository,
-      accountQueryRepository,
-    );
+  afterEach(async () => {
+    await backup.restore();
   });
 
   describe('createAccount', () => {
@@ -81,84 +90,74 @@ describe('Account Service', () => {
   describe('findAccount', () => {
     it('id로 계정을 검색한다.', async () => {
       // given
-      const accountId = initialAccount.id;
+      const account = await initializeAccount();
 
-      when(mockedAccountQueryRepository.findOneById(anything())).thenResolve(
-        initialAccount,
-      );
       // when
-      const findAccount = await accountService.findAccountById(accountId);
+      const findAccount = await accountService.findAccountById(account.id);
 
       // then
-      expect(findAccount.id).toBe(accountId);
-      expect(findAccount.email).toBe(initialAccount.email);
-      expect(findAccount.phone).toBe(initialAccount.phone);
-      expect(findAccount.password).toStrictEqual(expect.any(String));
+      expect(findAccount.id).toBe(account.id);
+      expect(findAccount.email).toBe(account.email);
+      expect(findAccount.phone).toBe(account.phone);
+      expect(findAccount.password).toBe(account.password);
     });
 
     it('email로 계정을 검색한다.', async () => {
       // given
-      const email = initialAccount.email;
-
-      when(
-        mockedAccountQueryRepository.findOneByEmail(anyString()),
-      ).thenResolve(initialAccount);
+      const account = await initializeAccount();
 
       // when
-      const findAccount = await accountService.findAccountByEmail(email);
+      const findAccount = await accountService.findAccountByEmail(
+        account.email,
+      );
+
       // then
-      expect(findAccount.id).toBe(initialAccount.id);
-      expect(findAccount.email).toBe(email);
-      expect(findAccount.phone).toBe(initialAccount.phone);
-      expect(findAccount.password).toStrictEqual(expect.any(String));
+      expect(findAccount.id).toBe(account.id);
+      expect(findAccount.email).toBe(account.email);
+      expect(findAccount.phone).toBe(account.phone);
+      expect(findAccount.password).toBe(account.password);
     });
   });
 
   describe('updateAccount', () => {
     it('계정 정보를 수정한다.', async () => {
       // given
-      const accountId = initialAccount.id;
+      const account = await initializeAccount();
+
       const updateAccountDto: UpdateAccountDto = {
         phone: faker.phone.number('+82 10-####-####'),
         password: faker.internet.password(),
       };
 
-      when(mockedAccountRepository.save(anyOfClass(Account))).thenResolve(
-        saveAccount({
-          id: accountId,
-          email: initialAccount.email,
-          phone: updateAccountDto.phone,
-          password: updateAccountDto.password,
-        }),
-      );
-
       // when
       const updateAccount = await accountService.updateAccount(
-        accountId,
+        account.id,
         updateAccountDto,
       );
 
+      const isValid = await updateAccount.validatePassword(
+        updateAccountDto.password,
+      );
+
       // then
-      expect(updateAccount.id).toBe(accountId);
-      expect(updateAccount.phone).toBe(updateAccountDto.phone);
-      expect(updateAccount.password).toStrictEqual(expect.any(String));
+      expect(updateAccount.id).toBe(account.id);
+      expect(updateAccount.phone).toBe(account.phone);
+      expect(isValid).toBe(true);
     });
 
     it('존재하지 않는 계정을 수정하려고 하면, NotFoundException을 던진다.', async () => {
       // given
-      const accountId = initialAccount.id;
+      const accountId = BigInt(faker.random.numeric());
+
       const updateAccountDto: UpdateAccountDto = {
         phone: faker.phone.number('+82 10-####-####'),
         password: faker.internet.password(),
       };
 
-      when(mockedAccountQueryRepository.findOneById(anything())).thenResolve(
-        null,
-      );
-
       // when
       const updateAccount = () =>
         accountService.updateAccount(accountId, updateAccountDto);
+
       // then
       expect(updateAccount).rejects.toThrowError(NotFoundException);
     });
@@ -167,19 +166,22 @@ describe('Account Service', () => {
   describe('deleteAccount', () => {
     it('계정을 id로 삭제한다.', async () => {
       // given
-      const accountId = initialAccount.id;
+      const account = await initializeAccount();
+
       // when
-      const deleteAccount = () => accountService.deleteAccountById(accountId);
+      await accountService.deleteAccountById(account.id);
+
+      const deleteAccount = await accountQueryRepository.findOneById(
+        account.id,
+      );
+
       // then
-      expect(deleteAccount).not.toThrow();
+      expect(deleteAccount).toBeNull();
     });
 
     it('존재하지 않는 계정을 삭제하려고 하면, NotFoundException을 던진다.', async () => {
       // given
-      const accountId = initialAccount.id;
-      when(mockedAccountQueryRepository.findOneById(anything())).thenResolve(
-        null,
-      );
+      const accountId = BigInt(faker.random.numeric());
 
       // when
       const deleteAccount = () => accountService.deleteAccountById(accountId);
