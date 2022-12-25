52f3b8c8d0d9a274fd4c8eaf251e1f1ff6fb45b9
kapri <44130355+kapri-sol@users.noreply.github.com>
2022-12-26 01:32:54 +0900

    order item 엔터티 생성

---
+++ src/entities/order-item.entity.ts
@@ -0,0 +1,66 @@
+import { BadRequestException } from '@nestjs/common';
+import { PrimaryGenerateBigintColumn } from '@src/utils/decorator/primary-generate-bigint-column.decorator';
+import { Column, Entity, JoinColumn, OneToMany, OneToOne } from 'typeorm';
+import { Order } from './order.entity';
+import { Product } from './product.entity';
+
+@Entity()
+export class OrderItem {
+  @PrimaryGenerateBigintColumn({
+    name: 'id',
+  })
+  private _id: bigint;
+
+  @OneToOne(() => Product, (product: Product) => product.orderItems)
+  @JoinColumn({
+    name: 'product_id',
+  })
+  private _product: Product;
+
+  @OneToMany(() => Order, (order: Order) => order.orderItems)
+  @JoinColumn({
+    name: 'order_id',
+  })
+  private _order: Order;
+
+  @Column({
+    name: 'count',
+  })
+  private _count: number;
+
+  get id() {
+    return this._id;
+  }
+
+  get product(): Product {
+    return this._product;
+  }
+
+  get order(): Order {
+    return this._order;
+  }
+
+  get count(): number {
+    return this._count;
+  }
+
+  /**
+   * 주문 항목 instance를 생성한다.
+   *
+   * @static
+   * @param {Product} product
+   * @param {number} count
+   * @return {*}
+   * @memberof OrderItem
+   */
+  static of(product: Product, count: number) {
+    if (count <= 0) {
+      throw new BadRequestException();
+    }
+
+    const orderItem = new OrderItem();
+    orderItem._product = product;
+    orderItem._count = count;
+    return orderItem;
+  }
+}
