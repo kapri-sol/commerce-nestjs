6c3c3ac9a4514d5b4be7ac1633168f60953604e4
kapri <44130355+kapri-sol@users.noreply.github.com>
2022-12-25 20:41:07 +0900

    모듈, 서비스 구현

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from '@src/entities/product.entity';
+import { SellerModule } from '../seller/seller.module';
+import { ProductQueryRepository } from './product.query-repository';
 import { ProductService } from './product.service';
 
 @Module({
-  providers: [ProductService],
+  imports: [TypeOrmModule.forFeature([Product]), SellerModule],
+  providers: [ProductQueryRepository, ProductService],
+  exports: [ProductQueryRepository, ProductService],
 })
 export class ProductModule {}
--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,6 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from '@src/entities/product.entity';
+import { Repository } from 'typeorm';
+import { SellerQueryRepository } from '../seller/seller.query-repository';
+import { CreateProductDto } from './dto/create.dto';
+import { UpdateProductDto } from './dto/update.dto';
+import { ProductQueryRepository } from './product.query-repository';
 
 @Injectable()
 export class ProductService {
-  constructor() {}
+  constructor(
+    private readonly sellerQueryRepository: SellerQueryRepository,
+    private readonly productQueryRepository: ProductQueryRepository,
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  findProductById(productId: bigint) {
+    return this.productQueryRepository.findOneById(productId);
+  }
+
+  findProductNameOrDescription(content: string) {
+    return this.productQueryRepository.findByNameOrDescription(content);
+  }
+
+  async createProduct(createProductDto: CreateProductDto) {
+    const seller = await this.sellerQueryRepository.findOneById(
+      createProductDto.sellerId,
+    );
+
+    if (!seller) {
+      throw new BadRequestException();
+    }
+
+    const { name, description, price } = createProductDto;
+
+    const product = Product.of(name, description, price, seller);
+
+    return this.productRepository.save(product);
+  }
+
+  async updateProduct(productId: bigint, updateProductDto: UpdateProductDto) {
+    const product = await this.productQueryRepository.findOneById(productId);
+
+    if (!product) {
+      throw new NotFoundException();
+    }
+
+    const { name, description, image, price } = updateProductDto;
+
+    product.update(name, description, price, image);
+
+    return this.productRepository.save(product);
+  }
+
+  async deleteProductById(productId: bigint) {
+    const product = await this.productQueryRepository.findOneById(productId);
+
+    if (!product) {
+      throw new NotFoundException();
+    }
+
+    await this.productRepository.softRemove(product);
+  }
 }
